5eeacd7722d26ca4c19515609bf8298dd3f0de58
Bradley Hintze <bhintze@mesa.lan>
2020-02-26 04:34:38 -0500

    Initial commit of simple python based container to interact with postgres.

---
+++ postgres_go/python/get_data.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+url = 'postgresql+psycopg2://user:pwd@database:5432/mydb'
+
+engine = create_engine(url)
+
+sql = 'SELECT * FROM family;'
+rows = engine.execute(sql)
+
+for r in rows : print(r)
